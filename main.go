2ebc6045f305e76d5dd265bc50bfb5a1fa488560
Zpuspokusumo <zpuspokusumo@gmail.com>
2024-04-18 00:54:46 +0700

    tests and more dirs. pass for relative and apparent calc

--- main.go
+++ main.go
@@ -13,13 +13,17 @@ import (
 func main() {
 	Wind := vecs.EuclideanVector{
 		Magnitude: 10,
-		Direction: math.Pi / 4,
+		Direction: math.Pi / 4, //pi/2 = 90
+		//Direction: math.Pi / 2, //pi/2 = 90
+		//Direction: 0,
 	}
 	Windy := wind.WindType{Density: 1, Vector: Wind}
 
 	Ship := vecs.EuclideanVector{
 		Magnitude: 10,
 		Direction: 3 * (math.Pi / 4),
+		//Direction: 0,
+		//Direction: math.Pi / 2,
 	}
 	Vessel := boat.Hull{Length: 40.0, Vector: Ship}
 	Shippy := boat.Boat{Sail: sail.NewCrabclaw(), Vessel: Vessel}
@@ -27,7 +31,11 @@ func main() {
 	//tests.TestCartesianInverseDir(Wind, Ship)
 	//tests.TestAddition(Wind, Ship)
 
-	fmt.Printf("expected pi/2 = %f\n\n", math.Pi/2)
+	//fmt.Println(Wind.ToDegrees(), Ship.ToDegrees())
+
+	fmt.Printf("expected pi/2 = %f\n\n", (math.Pi/2)*-1)
 
 	tests.TestBoatAngleinWind(Shippy, Windy)
+
+	tests.GPTTEST_RelativeWind()
 }
