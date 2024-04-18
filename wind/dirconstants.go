2f93e01472deb97e194008e224511763cedf7abd
Syahridan <syahridan@desktopip.com>
2024-04-18 18:17:23 +0700

    notes and dir constants from deg to rad

---
+++ wind/dirconstants.go
@@ -0,0 +1,14 @@
+package wind
+
+import "math"
+
+const (
+	DIR_0_DEGREES        float64 = 0
+	DIR_15_DEGREES       float64 = math.Pi / 6
+	DIR_22point5_DEGREES float64 = math.Pi / 4
+	DIR_30_DEGREES       float64 = math.Pi / 3
+	DIR_45_DEGREES       float64 = math.Pi / 2
+	DIR_60_DEGREES       float64 = (math.Pi / 3) * 2
+	DIR_75_DEGREES       float64 = (math.Pi / 6) * 5
+	DIR_90_DEGREES       float64 = math.Pi
+)
